bc27315e32c9d8debb0ee23b41d84bf5fbc3e964
root <root@docker-final-bkl.uqwegx5sd2bexlcz3dnn3jq3sa.bx.internal.cloudapp.net>
2024-02-07 16:31:15 +0000

    docker run

--- BlogServices/Program.cs
+++ BlogServices/Program.cs
@@ -83,11 +83,11 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
